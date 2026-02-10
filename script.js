b6b9ddc833c5b12c8090381769448ac05c159bc3
kalyugyodha765-lab <kalyugyodha765@gmail.com>
2026-02-10 16:20:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+function saveName() {
+
+  let name = document.getElementById("nameInput").value;
+
+  if(name.trim()===""){
+    alert("Enter your name");
+    return;
+  }
+
+  fetch("https://docs.google.com/forms/d/e/1FAIpQLSdfy4q1kE6U9rLUhn9Kn3WS4Gw9W6rgs8KIArSX6DybbxOnRQ/formResponse", {
+    method: "POST",
+    mode: "no-cors",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded"
+    },
+    body: "entry.273210999=" + encodeURIComponent(name)
+  });
+
+  alert("Saved successfully 😌");
+
+  window.location.href="page2.html";
+}
+
 function toggleCard(card){
   card.classList.toggle("active");
 }
@@ -7,9 +30,9 @@ function toggleCircle(box){
 }
 
 function goPage3(){
-  window.location.href = "page3.html";
+  window.location.href="page3.html";
 }
 
 function goPage4(){
-  window.location.href = "page4.html";
+  window.location.href="page4.html";
 }
